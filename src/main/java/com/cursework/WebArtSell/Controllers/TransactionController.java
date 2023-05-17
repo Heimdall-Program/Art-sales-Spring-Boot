a5cc635fa1f53d51e00ff9011293ad07b8967e83
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-17 21:24:00 +0300

    Add chart

--- src/main/java/com/cursework/WebArtSell/Controllers/TransactionController.java
+++ src/main/java/com/cursework/WebArtSell/Controllers/TransactionController.java
@@ -2,7 +2,6 @@ package com.cursework.WebArtSell.Controllers;
 
 
 import com.cursework.WebArtSell.Models.Transaction;
-import com.cursework.WebArtSell.Models.TransactionChartData;
 import com.cursework.WebArtSell.Services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class TransactionController {
@@ -17,12 +17,15 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+
     @GetMapping("/transaction_list")
     public String transactions(Model model) {
         List<Transaction> transactions = transactionService.findAll();
-        System.out.println(transactions);
+        Map<String, Double> chartData = transactionService.getMonthlySalesData();
         model.addAttribute("transactions", transactions);
+        model.addAttribute("chartData", chartData);
         return "transaction_list";
     }
-
 }
+
+
--- src/main/java/com/cursework/WebArtSell/Services/TransactionService.java
+++ src/main/java/com/cursework/WebArtSell/Services/TransactionService.java
@@ -5,7 +5,10 @@ import com.cursework.WebArtSell.Repo.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Service
 public class TransactionService {
@@ -16,4 +19,19 @@ public class TransactionService {
     public List<Transaction> findAll() {
         return transactionRepository.findAll();
     }
+
+    public Map<String, Double> getMonthlySalesData() {
+        List<Transaction> transactions = findAll();
+
+        Map<String, Double> salesData = new TreeMap<>();
+
+        for (Transaction transaction : transactions) {
+            String monthYear = transaction.getPurchaseDate().format(DateTimeFormatter.ofPattern("MM-yyyy"));
+
+            salesData.put(monthYear, salesData.getOrDefault(monthYear, 0.0) + transaction.getSum());
+        }
+
+        return salesData;
+    }
+
 }
