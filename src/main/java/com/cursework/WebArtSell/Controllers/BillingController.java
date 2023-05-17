b155ae733d656f58f058ca39046dba507db4b155
Даниил <90754791+Heimdall-Program@users.noreply.github.com>
2023-05-17 23:54:31 +0300

    Add billing

--- src/main/java/com/cursework/WebArtSell/Controllers/BillingController.java
+++ src/main/java/com/cursework/WebArtSell/Controllers/BillingController.java
@@ -1,15 +1,61 @@
 package com.cursework.WebArtSell.Controllers;
 
+import com.cursework.WebArtSell.Models.Product;
+import com.cursework.WebArtSell.Models.Transaction;
+import com.cursework.WebArtSell.Repo.ProductRepository;
+import com.cursework.WebArtSell.Repo.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 public class BillingController {
 
-    @GetMapping("/billing")
-    public String authorisationModel(Model model) {
-        model.addAttribute("users", "");
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @GetMapping("/billing/{id}")
+    public String authorisationModel(@PathVariable Long id, Model model) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+
+            Transaction transaction = new Transaction();
+            transaction.setBuyerId("yourBuyerId");
+            transaction.setSellerId("yourSellerId");
+            transaction.setPurchaseDate(LocalDateTime.now());
+            transaction.setSum(Double.valueOf(product.get().getPrice().toString()));
+            model.addAttribute("transaction", transaction);
+        } else {
+            return "redirect:/billing";
+        }
+
         return "billing";
     }
+
+    @PostMapping("/billing-buy")
+    public String processTransaction(@ModelAttribute Transaction transaction, Model model) {
+        try {
+            transactionRepository.save(transaction);
+        } catch (Exception e) {
+            model.addAttribute("error", "Ошибка");
+            model.addAttribute("transaction", new Transaction());
+            return "billing";
+        }
+
+        return "redirect:/main-user";
+    }
+
 }
+
